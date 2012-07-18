41d6e6dc4ad15819e0bd3cce0d516007a5d31a32
Richard Brunt <r.brunt@gmail.com>
2012-07-18 18:58:42 +0100

    added "fork me on github" ribbon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<title>Gravatar Test</title>
 </head>
 <body>
+	<a href="https://github.com/rbrunt/gravatar-getter"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 <form action="getavatar.php" method="post">
 	<div>
 		<label for="email">Enter Email adress to get your gravatar:</label>
