dc870dc7f9436d1c32f043e91a02a690e1dd2ac8
Richard Brunt <r.brunt@gmail.com>
2012-07-18 19:04:35 +0100

    added tab index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <form action="getavatar.php" method="post">
 	<div>
 		<label for="email">Enter Email adress to get your gravatar:</label>
-		<input type="email" name="email" placeholder="Enter email address"><br>
+		<input type="email" name="email" placeholder="Enter email address" tabindex="1"><br>
 		<label for="size">Enter the desired size of your gravatar:</label>
 		<input type="number" name="size" min="1" max="512" value="100"/>
 		<br><input type="submit" value="Submit">
