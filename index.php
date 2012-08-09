bbfa66433e210724e718fce9ba0360e606b35f8e
Richard Brunt <r.brunt@gmail.com>
2012-08-09 14:52:54 +0200

    Update tabbing
    fixed broken tab order in form elements

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 		<label for="email">Enter Email adress to get your gravatar:</label>
 		<input type="email" name="email" placeholder="Enter email address" tabindex="1"><br>
 		<label for="size">Enter the desired size of your gravatar:</label>
-		<input type="number" name="size" min="1" max="512" value="100"/>
-		<br><input type="submit" value="Submit">
+		<input type="number" name="size" min="1" max="512" value="100" tabindex="2"/>
+		<br><input type="submit" value="Submit" tabindex="3">
 	</div>
 </form>
 </body>
